ae4519c69df9b85ae0bba2174d0f645964fdac8e
Amilson  Junior <amilson_junior@outlook.com>
2016-06-18 02:57:44 -0300

    Graph added

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Main : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        Graph graph = new Graph(4);
+
+        graph.Initialize();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        
 	}
 }
