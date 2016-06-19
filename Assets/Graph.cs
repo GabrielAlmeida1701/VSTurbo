03a693e0c0bfbd1a3cc43bbdb8db851ae2fb587a
Amilson  Junior <amilson_junior@outlook.com>
2016-06-19 00:21:27 -0300

    adjacent matrix deleted
    adjacent list is now the main data structure

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -37,13 +37,13 @@ namespace Assets
     {
         public List<Node> listNodes; //"cidades"
 
-        int[,] matrizAdj;
+        //int[,] matrizAdj;
         int nodesSize;
 
         public Graph(int size)
         {
             nodesSize = size;
-            matrizAdj = new int[size, size];
+            //matrizAdj = new int[size, size];
             listNodes = new List<Node>();
         }
 
@@ -67,82 +67,165 @@ namespace Assets
 
         private void GenGraph()
         {
-            for(int i = 0; i < nodesSize; i++)
+            for (int i = 0; i < nodesSize; i++)
             {
-                for(int j = 0; j < nodesSize; j++)
-                {
-                    if (i == j) matrizAdj[i, j] = 0;
-                    else
-                    {
-                        int index = Random.Range(0, 2);
-                        matrizAdj[i, j] = index;
-                        matrizAdj[j, i] = index;
-                    }
-                }
+                listNodes.Add(createNode(i));
             }
 
-            TransformToList();
+            AddEdge(0, 1, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(0, 7, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(0, 12, ROAD_TYPE.DIRT);
+            AddEdge(0, 11, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(1, 0, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(1, 2, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(1, 7, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(1, 11, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(2, 1, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(2, 3, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(2, 7, ROAD_TYPE.DIRT);
+            AddEdge(2, 12, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(3, 2, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(3, 6, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(3, 5, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(3, 4, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(3, 9, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(3, 13, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(3, 8, ROAD_TYPE.DIRT);
+
+            AddEdge(4, 3, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(4, 5, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(4, 6, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(4, 9, ROAD_TYPE.ASPHALT_GOOD);
+
+            AddEdge(5, 3, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(5, 4, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(5, 6, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(5, 9, ROAD_TYPE.DIRT);
+            AddEdge(5, 10, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(6, 3, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(6, 4, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(6, 5, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(6, 10, ROAD_TYPE.ASPHALT_GOOD);
+
+            AddEdge(7, 1, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(7, 0, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(7, 2, ROAD_TYPE.DIRT);
+            AddEdge(7, 11, ROAD_TYPE.DIRT);
+            AddEdge(7, 12, ROAD_TYPE.ASPHALT_GOOD);
+            
+            AddEdge(8, 3, ROAD_TYPE.DIRT);
+            AddEdge(8, 9, ROAD_TYPE.DIRT);
+            AddEdge(8, 12, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(8, 13, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(8, 15, ROAD_TYPE.DIRT);
+
+            AddEdge(9, 3, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(9, 4, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(9, 5, ROAD_TYPE.DIRT);
+            AddEdge(9, 8, ROAD_TYPE.DIRT);
+            AddEdge(9, 10, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(9, 14, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(9, 13, ROAD_TYPE.DIRT);
+
+            AddEdge(10, 5, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(10, 6, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(10, 9, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(10, 14, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(10, 17, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(11, 0, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(11, 1, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(11, 7, ROAD_TYPE.DIRT);
+            AddEdge(11, 12, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(11, 15, ROAD_TYPE.ASPHALT_GOOD);
+
+            AddEdge(12, 0, ROAD_TYPE.DIRT);
+            AddEdge(12, 2, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(12, 7, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(12, 8, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(12, 11, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(12, 13, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(12, 15, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(13, 3, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(13, 8, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(13, 9, ROAD_TYPE.DIRT);
+            AddEdge(13, 12, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(13, 14, ROAD_TYPE.DIRT);
+            AddEdge(13, 15, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(13, 16, ROAD_TYPE.ASPHALT_BAD);
+
+            AddEdge(14, 9, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(14, 10, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(14, 17, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(14, 13, ROAD_TYPE.DIRT);
+            AddEdge(14, 16, ROAD_TYPE.DIRT);
+
+            AddEdge(15, 11, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(15, 12, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(15, 8, ROAD_TYPE.DIRT);
+            AddEdge(15, 13, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(15, 16, ROAD_TYPE.DIRT);
+
+            AddEdge(16, 13, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(16, 14, ROAD_TYPE.DIRT);
+            AddEdge(16, 15, ROAD_TYPE.DIRT);
+            AddEdge(16, 17, ROAD_TYPE.ASPHALT_GOOD);
+
+            AddEdge(17, 10, ROAD_TYPE.ASPHALT_BAD);
+            AddEdge(17, 14, ROAD_TYPE.ASPHALT_GOOD);
+            AddEdge(17, 16, ROAD_TYPE.ASPHALT_GOOD);
         }
 
-        public void AddEdge(int S, int D)
+        public void AddEdge(int S, int D, ROAD_TYPE rType)
         {
             Edge edge = new Edge();
             edge.adjacent = listNodes[S];
             edge.weight = Random.Range(5, 10);
-            ROAD_TYPE rtype = (ROAD_TYPE)Random.Range((int)ROAD_TYPE.ASPHALT_GOOD, (int)ROAD_TYPE.BRIDGE);
-            edge.road_type = rtype;
+            edge.road_type = rType;
 
             listNodes[D].adjacents.Add(edge);
         }
 
         private void TransformToList()
         {
-            for(int i = 0; i < nodesSize; i++)
-            {
-                listNodes.Add(createNode(i));
-            }
+            //for(int i = 0; i < nodesSize; i++)
+            //{
+            //    listNodes.Add(createNode(i));
+            //}
 
 
-            for (int i = 0; i < nodesSize; i++)
-            {
-                for (int j = 0; j < nodesSize; j++)
-                {
-                    if (matrizAdj[i, j] == 1)
-                    {
-                        if (listNodes[j].adjacents.Count < 5)
-                        {
-                            AddEdge(i, j);
-                        }
-                    }
-                }
-            }
+            //for (int i = 0; i < nodesSize; i++)
+            //{
+            //    for (int j = 0; j < nodesSize; j++)
+            //    {
+            //        if (matrizAdj[i, j] == 1)
+            //        {
+            //            if (listNodes[j].adjacents.Count < 5)
+            //            {
+            //                AddEdge(i, j);
+            //            }
+            //        }
+            //    }
+            //}
         }
 
+        //Apenas para teste
         private void WriteAllInAFile()
         {
 			//using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\andrey\Desktop\ResultGraph.txt"))
 			using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\amils\Desktop\ResultGraph.txt"))
             {
-                for (int i = 0; i < nodesSize; i++)
+                for (int i = 0; i < listNodes.Count; i++)
                 {
-                    file.WriteLine();
-                    for (int j = 0; j < nodesSize; j++)
-                    {
-                        file.Write(" {0}", matrizAdj[i,j]);
-                    }
-                }
-
-                file.WriteLine();
-
-                for (int i = 0; i < 1; i++)
-                {
-					file.WriteLine ();
+					file.Write("Vertice[{0}] => ", listNodes[i].id);
                     for (int j = 0; j < listNodes[i].adjacents.Count; j++)
-						//(id_vert road_t weight)
                     {
-                        file.Write("(" + listNodes[i].adjacents[j].adjacent.id + " ");
-						file.Write ((int)listNodes [i].adjacents [j].road_type + " ");
-						file.Write (listNodes [i].adjacents [j].weight + ")");
+                        file.Write(" " + listNodes[i].adjacents[j].adjacent.id);
                     }
                     file.WriteLine();
                 }
